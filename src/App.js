ca24c28fc51d746f54b102d0e2c2221c190d1a7e
ANUJ BHASIN <anujbhasin25@gmail.com>
2022-08-25 12:14:07 +0530

    Created Links for Router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
 import AboutPage from './pages/AboutPage';
 import FeedbackData from './data/FeedbackData';
 
@@ -40,9 +41,9 @@ function App() {
               </>
             }
           ></Route>
-
           <Route path='/about' element={<AboutPage />} />
         </Routes>
+        <AboutIconLink />
       </div>
     </Router>
   );
